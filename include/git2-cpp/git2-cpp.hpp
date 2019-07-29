a2a407cac5714b2b4e12303cf4009b5f542f8ee7
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-07-30 07:00:28 +0900

    Create some headers

--- include/git2-cpp/git2-cpp.hpp
+++
@@ -1,4 +0,0 @@
-#ifndef GIT2-CPP_HPP
-#define GIT2-CPP_HPP
-
-#endif // !GIT2-CPP_HPP
---
+++ include/git2-cpp/git2.hpp
@@ -0,0 +1,10 @@
+#ifndef GIT2_CPP_HPP
+#define GIT2_CPP_HPP
+
+#include <git2-cpp/config.hpp>
+#include <git2-cpp/describe.hpp>
+#include <git2-cpp/exception.hpp>
+#include <git2-cpp/global.hpp>
+#include <git2-cpp/repository.hpp>
+
+#endif	// !GIT2_CPP_HPP
